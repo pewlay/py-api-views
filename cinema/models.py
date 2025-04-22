64210cb5916f7cba77f9cd1933e0b9a9f1bd11d1
Polina Haltseva <polinayut@gmail.com>
2025-04-22 19:39:59 +0300

    'Solution'

--- cinema/models.py
+++ cinema/models.py
@@ -1,10 +1,27 @@
 from django.db import models
 
 
+class Actor(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+
+
+class Genre(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+
 class Movie(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
     duration = models.IntegerField()
+    actors = models.ManyToManyField(Actor, related_name="actors")
+    genres = models.ManyToManyField(Genre, related_name="genres")
 
     def __str__(self):
         return self.title
+
+
+class CinemaHall(models.Model):
+    name = models.CharField(max_length=255)
+    rows = models.IntegerField()
+    seats_in_row = models.IntegerField()
--- cinema/urls.py
+++ cinema/urls.py
@@ -1,10 +1,26 @@
 from django.urls import path
+from rest_framework import routers
+from cinema.views import (
+    GenreDetail,
+    GenreList,
+    ActorList,
+    ActorDetail,
+    CinemaHallViewSet,
+    MovieViewSet
+)
 
-from cinema.views import movie_list, movie_detail
+
+router = routers.DefaultRouter()
+
+router.register("movies", MovieViewSet)
 
 urlpatterns = [
-    path("movies/", movie_list, name="movie-list"),
-    path("movies/<int:pk>/", movie_detail, name="movie-detail"),
-]
+    path("genres/", GenreList.as_view(), name="genre-list"),
+    path("genres/<int:pk>/", GenreDetail.as_view(), name="genre-detail"),
+    path("actors/", ActorList.as_view(), name="actor-list"),
+    path("actors/<int:pk>/", ActorDetail.as_view(), name="actor-detail"),
+    path("cinema_halls/", CinemaHallViewSet.as_view({"get": "list", "post": "create"}), name="hall-list"),
+    path("cinema_halls/<int:pk>/", CinemaHallViewSet.as_view({"get": "retrieve", "put": "update", "patch": "partial_update", "delete": "destroy"}), name="hall-detail"),
+] + router.urls
 
 app_name = "cinema"
